984c1d4418e18d591a5fbfb06f38dbc11dacde0f
CrooKameron <85321458+CrooKameron@users.noreply.github.com>
2022-09-17 13:18:25 +0300

    calc-app

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons =         document.querySelectorAll('[data-number]')
+const operationButtons =      document.querySelectorAll('[data-operation]')
+const equalsButton =          document.querySelectorAll('[data-equals]')
+const deletesButton =         document.querySelectorAll('[data-delete]')
+const allClearButton =        document.querySelectorAll('[data-allclear]')
+const previousOperandButton = document.querySelectorAll('[data-previous-operand]')
+const currentOperandButton =  document.querySelectorAll('[data-current-operand]')
