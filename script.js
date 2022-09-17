6a2f308f002f72aaa7082d784a33ac6bc14d5850
CrooKameron <85321458+CrooKameron@users.noreply.github.com>
2022-09-17 18:12:55 +0300

    final-version

--- script.js
+++ script.js
@@ -11,22 +11,80 @@ class Calculator {
         this.operation = undefined
     }
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
     appendNumber(number){
-        this.currentOperand = number
+        if(number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString() 
     }
     chooseOperation(operation){
-
+        if (this.currentOperand === '')  return; 
+        if(this.previousOperand != ''){
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
     compute(){
+        let computation
+        const prev = parseFloat(this.currentOperand)
+        const current = parseFloat(this.previousOperand)
+        if(isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+
+            case '-':
+                computation = prev - current
+                break
+
+            case 'x':
+                computation = prev * current
+                break
+
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getDisplayNumber(number){
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)){
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0 })    
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        } else{
+            return integerDisplay
+        }
 
     }
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = 
+             `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
 
+const evrything = document
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -44,3 +102,152 @@ numberButtons.forEach(button => {
         calculator.updateDisplay()
     })
 })
+
+
+
+operationButtons.forEach(button => {
+    button.addEventListener('click',()=>{
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+ 
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// main keys
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "Enter") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "Backspace") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == " ") {
+        console.log(event.key)
+        console.log("space key is invisible")
+    }
+})
+
+
+// symbols
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "+") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "-") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "*" || event.key == "x") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "/") {
+        console.log(event.key)
+    }
+})
+
+
+
+// numbers
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "0") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "1") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "2") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "3") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "4") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "5") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "6") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "7") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "8") {
+        console.log(event.key)
+    }
+})
+window.addEventListener('keydown', (event) =>{
+    if (event.key == "9") {
+        console.log(event.key)
+    }
+})
+
+
+
+
+
